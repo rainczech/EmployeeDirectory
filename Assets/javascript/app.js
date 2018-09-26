b009e8b8d448fa40166293fdcbbfeb880ffa0125
Grace Ho <rainczech@gmail.com>
2018-09-26 16:35:36 -0500

    added render functionality

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,18 +1,21 @@
 const empCard = $('.content');
 
+
+// render function for view
 const render = function (){
   $('.content').empty();
   for (let i=0; i<employeeList.length; i++){
-    const newCard = $('<div>');
-    newCard.text(`<p>${employeeList[i].name}</p><p>${employeeList[i].officeNum}</p><p>${employeeList[i].phoneNum}</p>`)
-    newCard.attr('class', 'card')
-    empCard.append(newCard)
-    
-    //   for( let i = 0; i < studentList.length; i++ ) {
-    //     $('.content').append(`<p>${studentList[i]}</p>`);
-    //   }
-    // }
-    
-    render();
+    const newCard = (`<div class="card">
+    <p>${employeeList[i].name}</p>
+    <p>${employeeList[i].officeNum}</p>
+    <p>${employeeList[i].phoneNum}</p>
+    </div>`);
+    empCard.append(newCard);
   }
 }
+
+
+
+// buttons that target functions
+$("#view").on("click", render);
+
